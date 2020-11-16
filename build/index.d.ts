795dfca5f8776e9cdc710fccbfd971a5ba33e0a0
Simonas Karuzas <simonas@not.cat>
2020-11-16 18:34:35 +0200

    chore: Removing build

--- build/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { MyAgentPlugin } from './agent/my-plugin';
-export { KeyManagementSystem } from './key-manager/key-management-system';
-export { KeyStore } from './key-manager/key-store';
-export { SecretBox } from './key-manager/secret-box';
-export { IdentityProvider } from './identity-manager/identity-provider';
-export { IdentityStore } from './identity-manager/identity-store';
--- build/key-manager/secret-box.d.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractSecretBox } from 'daf-key-manager';
-export declare class SecretBox extends AbstractSecretBox {
-    private secretKey;
-    constructor(secretKey: string);
-    encrypt(message: string): Promise<string>;
-    decrypt(encryptedMessageHex: string): Promise<string>;
-}
